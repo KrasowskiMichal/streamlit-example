cc67c345c23771a67ff3fccb9974f65f84508485
KrasowskiMichal <michal.krasowski@gmail.com>
2023-06-15 07:06:18 +0200

    hello world

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,19 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# import streamlit module
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# title of the app
+st.title("Prosta Aplikacja Streamlit")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# text input field
+user_input = st.text_input("Wpisz coś tutaj")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# display user input
+st.write(f"Twoje wprowadzone dane: {user_input}")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# multiple options select box
+option = st.selectbox(
+    'Wybierz jedną z opcji:',
+     ('Opcja 1', 'Opcja 2', 'Opcja 3'))
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# display selected option
+st.write(f"Twoja wybrana opcja: {option}")
